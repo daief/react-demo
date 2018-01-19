17804574ad0b8842932b53fef22ad8b76f5c215b
daief <1437931235@qq.com>
2018-01-19 14:23:58 +0800

    cm demo1

--- src/demo01/index.js
+++ src/demo01/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
+/**
+ * react 是核心，react-dom 是提供与 DOM 相关的功能（见入口文件）
+ */
 export default class Demo01 extends React.Component {
   render() {
-    return <div>demo01</div>
+    const names = ['Alice', 'Bob', 'Kate']
+    // 将模板存储在 js 数组
+    const htmls = [
+      <p key={1}>hello world</p>,
+      <p key={2}>hello react</p>
+    ]
+    // jsx，支持 html 和 js 得混写，html 直接写在 js 之中
+    // 遇到 HTML 标签（以 < 开头），就用 HTML 规则解析；遇到代码块（以 { 开头），就用 JavaScript 规则解析
+    return (
+      <div>
+        demo01
+        {
+          // Each child in an array or iterator should have a unique "key" prop.
+          names.map(_ => <div key={_}>Hello, {_}!</div>)
+        }
+        {/* 自动展开数组 */}
+        { htmls }
+      </div>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import {render} from 'react-dom'
 
 import App from './demo01'
 
-render(<App/>, document.querySelector('#app'))
+// ReactDOM.render 是 React 的最基本方法，用于将模板转为 HTML 语言，并插入指定的 DOM 节点。
+render(
+  <App/>,
+  document.querySelector('#app')
+)
