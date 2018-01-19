2abd3f699311f1aeb6bd04d22493b9e96dd413e8
daief <1437931235@qq.com>
2018-01-19 16:09:25 +0800

    cm

--- src/demo04/index.js
+++ src/demo04/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+/**
+ * 从父组件访问子节点的 DOM 节点，建议在子节点上暴露一个特殊的属性。
+ * 子节点将会获得一个函数属性，并将其作为 ref 属性附加到 DOM 节点。
+ * 这允许父代通过中间件将 ref 回调给子代的 DOM 节点。
+ */
 class Child extends React.Component {
   render() {
     return (
