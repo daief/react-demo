24681fe1651d633f00003240c8f2efbea09f2fbb
daief <1437931235@qq.com>
2018-01-19 11:08:06 +0800

    demo01 init

---
+++ src/demo01/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Demo01 extends React.Component {
+  render() {
+    return <div>demo01</div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './index.css'
 import React from 'react'
 import {render} from 'react-dom'
 
-import App from './App'
+import App from './demo01'
 
 render(<App/>, document.querySelector('#app'))
